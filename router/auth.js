b79d67b676b9825550bf2069db8de09adf0f5409
saleemglobal <saleem65.teacher@gmail.com>
2023-04-08 16:41:50 +0500

    code updated

--- router/auth.js
+++ router/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 require("../Db/connection");
+
 const User = require("../Model/userSchema");
 
 // router.get("/", (req, res) => {
@@ -9,50 +10,65 @@ const User = require("../Model/userSchema");
 // });
 
 //promise return method start from here...
-router.post("/MernSignup", (req, res) => {
-	// console.log(req.body);
-	// res.json({ message: req.body });
-	const { name, email, phone, work, passwd, cpasswd } = req.body;
-	// console.log(name);
-	// console.log(email);
-	// res.json({ message: email,name });
-	if (!name || !email || !phone || !work || !passwd || !cpasswd) {
-		return res.json({ messege: "please fill the required data..." });
-	}
-	console.log(req.body);
-	User.findOne({ email: email })
-		.then((emailExist) => {
-			if (emailExist) {
-				return res.status(422).json({ message: "user already exist." });
-			}
-
-			const adduser = new User({ name, email, phone, work, passwd, cpasswd });
-			adduser
-				.save()
-				.then(() => {
-					res.status(201).json({ message: "user registered successfully." });
-					console.log("user registered successfully.");
-				})
-				.catch((error) => {
-					res.status(500).json({ message: "registration failed", error });
-					console.log("Registratin failed.", error);
-				});
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-});
-// router.post("./MernLogin", (req, res) => {
+// router.post("/MernSignup", (req, res) => {
 // 	const { name, email, phone, work, passwd, cpasswd } = req.body;
-// 	if (!email || !passwd || !cpasswd) {
-// 		return res.json({ rmessage: "please fill the empty field. " });
+// 	if (!name || !email || !phone || !work || !passwd || !cpasswd) {
+// 		return res.json({ messege: "please fill the required data..." });
 // 	}
+// 	console.log(req.body);
 // 	User.findOne({ email: email })
-// 		.than()
+// 		.then((emailExist) => {
+// 			if (emailExist) {
+// 				return res.status(422).json({ message: "user already exist." });
+// 			}
+
+// 			const adduser = new User({ name, email, phone, work, passwd, cpasswd });
+// 			adduser
+// 				.save()
+// 				.then(() => {
+// 					res.status(201).json({ message: "user registered successfully." });
+// 					console.log("user registered successfully.");
+// 				})
+// 				.catch((error) => {
+// 					res.status(500).json({ message: "registration failed", error });
+// 					console.log("Registratin failed.", error);
+// 				});
+// 		})
 // 		.catch((error) => {
 // 			console.log(error);
 // 		});
 // });
+
 //promise return method ends  here...
+
+// async method starts from here......
+
+router.post("/MernSignup", async (req, res) => {
+	console.log("response from server auth", req.body);
+
+	const { name, email, phone, work, passwd, cpasswd } = req.body;
+
+	if (!name || !email || !phone || !work || !passwd || !cpasswd) {
+		return res.json({ message: "fill the empty field please..." });
+	}
+	try {
+		const emailExist = await User.findOne({ email: email });
+		if (emailExist) {
+			return res.status(422).json({ message: "email alredy exist." });
+		}
+		const user = new User({ name, email, phone, work, passwd, cpasswd });
+
+		const registerUser = await user.save();
+		res.status(201).json({ message: "registration successful." });
+		console.log(`${user}   registration successful.`);
+		console.log(registerUser);
+	} catch {
+		(error) => {
+			console.log(error);
+		};
+	}
+});
+
+// async method ends here......
 
 module.exports = router;
